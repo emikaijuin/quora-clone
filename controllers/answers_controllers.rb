cb7e2d22a8ae0d688569221fbb8078d7f16885cf
Emika Ijuin <emikaijuin@gmail.com>
2018-02-08 06:49:59 +0000

    Fixed glitch in answer controller and show page preventing answers from being submitted to database.

--- controllers/answers_controllers.rb
+++ controllers/answers_controllers.rb
@@ -3,11 +3,17 @@ get "/questions/:id/answer/new" do
     erb :"questions/answers/new"
 end
 
-post "/questions/:id/answer/" do
+post "/questions/:id/answer" do
     
     @question = Question.find(params[:id])
     @answer = Answer.create(answer: params[:answer], votes: 0, question_id: @question.id)
     
-    redirect "/question/#{@question.id}"
+    redirect "/questions/#{@question.id}"
     
+end
+
+get "/questions/:id/answer/:answer_id" do
+    @answer = Answer.find(params[:answer_id])
+    @answer.vote += 1
+    @answer.save
 end
